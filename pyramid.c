33bd7475b759ad1f792e733fa983f9daa24f5bae
Blue-Panther18 <olabisiabdusshakur01@gmail.com>
2024-10-08 22:33:53 +0100

    added the pyramid print codes

---
+++ pyramid.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - calls the pyramid function.
+ *
+ * Return: 0
+ *
+ * @int: N param.
+ */
+void print_pyramid(int);
+
+int main(void)
+{
+	int n;
+
+	printf("Enter the number pyramid rows: ");
+	scanf("%d", &n);
+
+	print_pyramid(n);
+
+	return (0);
+}
+
+/**
+ * print_pyramid - prints * n rows times.
+ *
+ * @n: n param
+ *
+ */
+void print_pyramid(int n)
+{
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = n - i; j > 0; j--)
+			printf(" ");
+		for (int j = 0; j < i; j++)
+			printf("*");
+		printf("\n");
+	}
+}
